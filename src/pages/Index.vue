6af7a2ba1d28d6182bbf0ebd85fc8df09819e6bb
张洋雷 <1258157385@qq.com>
2022-05-31 14:13:48 +0800

    versel config

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout>
     <!-- Page Header-->
     <header class="masthead" :style="{
-      backgroundImage: `url(http://${GRIDSOME_API_URL}${general.cover.url})`
+      backgroundImage: `url(${general.cover.url})`
     }">
       <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
